c938b1e9448004514406b6c289bc91d598414673
Winry <winry.tian@gmail.com>
2022-03-18 14:01:01 -0700

    add template project with Kiwi's debug corner

---
+++ sketch.js
@@ -0,0 +1,62 @@
+/**
+ *  @author
+ *  @date 2022.03.
+ *
+ *
+ */
+let font
+let instructions
+
+
+function preload() {
+    font = loadFont('data/consola.ttf')
+}
+
+
+function setup() {
+    let cnv = createCanvas(600, 300)
+    cnv.parent('#canvas')
+    colorMode(HSB, 360, 100, 100, 100)
+    textFont(font, 14)
+
+    /* initialize instruction div */
+    instructions = select('#ins')
+    instructions.html(`<pre>
+        [1,2,3,4,5] → no function
+        z → freeze sketch</pre>`)
+}
+
+
+function draw() {
+    background(234, 34, 24)
+
+
+
+    displayDebugCorner()
+}
+
+
+/** 🧹 shows debugging info using text() 🧹 */
+function displayDebugCorner() {
+    const LEFT_MARGIN = 10
+    const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
+    const LINE_SPACING = 2
+    const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
+    fill(0, 0, 100, 100) /* white */
+    strokeWeight(0)
+
+    text(`frameCount: ${frameCount}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
+    text(`frameRate: ${frameRate().toFixed(1)}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET)
+}
+
+function keyPressed() {
+    /* stop sketch */
+    if (key === 'z') {
+        noLoop()
+        instructions.html(`<pre>
+            sketch stopped</pre>`)
+        cursor()
+    }
+}
